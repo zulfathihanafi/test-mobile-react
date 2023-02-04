971aed8e21f1c4802602b71ec9614b88cce96fc3
zulfathihanafi <fathiimran7@gmail.com>
2023-02-04 08:23:10 +0800

    enabled persistence ?

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,19 +19,23 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+const db = getFirestore(app)
 
-enableIndexedDbPersistence(db)
+enableIndexedDbPersistence(db).then((connection)=>{
+    console.log('enabled')
+})
   .catch((err) => {
       if (err.code == 'failed-precondition') {
+        console.log('failed-precondition')
           // Multiple tabs open, persistence can only be enabled
           // in one tab at a a time.
           // ...
       } else if (err.code == 'unimplemented') {
+        console.log('unimplemented')
           // The current browser does not support all of the
           // features required to enable persistence
           // ...
       }
   });
 
-export { app }
+export { app,db }
