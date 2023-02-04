f1f681319ce37c808ecf84aba80839e14a70f00e
zulfathihanafi <fathiimran7@gmail.com>
2023-02-04 08:17:50 +0800

    enable persistence ?

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { enableIndexedDbPersistence, getFirestore, initializeFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB36udG_e-l7ELNxA5xqeAkVB9xa9oXLEg",
@@ -20,4 +20,18 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app)
+
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
 export { app }
